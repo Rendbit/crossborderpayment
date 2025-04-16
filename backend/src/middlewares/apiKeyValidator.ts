637a94736be5c4aad3d52af42678f93593fc701b
devcharlezen <devcharlezen@gmail.com>
2025-04-16 12:18:29 +0100

    chore: Changed port and routes

--- backend/src/middlewares/apiKeyValidator.ts
+++ backend/src/middlewares/apiKeyValidator.ts
@@ -9,10 +9,7 @@ const { API_KEY } = process.env;
 
 export const apiKeyValidator = (req: any, res: any, next: any) => {
   const clientKey = req.headers["x-api-key"];
-  if (
-    req.path.startsWith("/crossborderpayment/api/docs") ||
-    req.path.includes("/docs")
-  ) {
+  if (req.path.includes("/docs")) {
     return next();
   }
   if (!clientKey) {
