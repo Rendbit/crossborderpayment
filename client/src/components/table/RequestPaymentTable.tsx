f680419995c0360bc0cce9f67b8f67e76b40a038
franklin-raph <igboekwulusifranklin@gmail.com>
2025-12-26 19:55:41 +0100

    worked ont the request payment api integration

--- client/src/components/table/RequestPaymentTable.tsx
+++ client/src/components/table/RequestPaymentTable.tsx
@@ -478,8 +478,8 @@ const RequestPaymentTable: React.FC<TransactionTableProps> = ({
               className="px-2 py-2 border cursor-pointer focus:outline-0 focus:border-[#0E7BB2] border-[#E2E4E9] dark:border-gray-700 rounded-lg text-sm bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100"
             >
               <option value="all">All</option>
-              <option value="send">Crypto</option>
-              <option value="receive">Fiat</option>
+              <option value="crypto">Crypto</option>
+              <option value="fiat">Fiat</option>
             </select>
 
             <button
